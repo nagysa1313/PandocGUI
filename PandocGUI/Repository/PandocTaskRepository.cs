0f68f1ac16c19988b6d66b08622f8bade62f62d9
nagysa1313 <nagysa1313@gmail.com>
2014-04-15 20:00:07 +0200

    Started to implement xml based data storage

---
+++ PandocGUI/Repository/PandocTaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using PandocGUI.Model;
+using SharpRepository.Repository;
+using SharpRepository.XmlRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PandocGUI.Repository
+{
+    public class PandocTaskRepository : XmlRepository<PandocTask>
+    {
+        static PandocTaskRepository()
+        {
+            
+        }
+    }
+}
